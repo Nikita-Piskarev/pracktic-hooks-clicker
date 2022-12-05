803f478729404502df46939ef3fc2628df18b0fb
Nikita Piskarev <npiskarev23@gmail.com>
2022-12-05 16:21:32 +0200

    Add btn annd innterval commit
    Add interval, input, btn auto click. Fix changeCounter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   let [currentNumber, setCurrentNumber] = useState(0);
   const [counterNumber, setCounterNumber] = useState(1);
+  const [isIncrease, setIsIncrease] = useState(true);
+  const [isAutoClick, setIsAutoClick] = useState(true);
+  const [intervalTime, setIntervalTime] = useState(1000);
 
-  const textIncOrDec = counterNumber >= 0 ? "Increase" : "Decrease";
+  const textPages = isIncrease ? "Increase" : "Decrease";
+  const textBtn = isIncrease ? "Decrease" : "Increase";
 
-  const handleChange = (e) => {
-    const {
-      target: { value },
-    } = e;
-    setCounterNumber(value);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeСounter();
+    }, intervalTime);
+    return () => clearInterval(interval);
+  });
+
+  const handleChange = (e) => setCounterNumber(e.target.value);
+  const hendleIntervalTime = (e) => setIntervalTime(e.target.value);
+
   const changeСounter = () => {
-    setCurrentNumber((currentNumber += +counterNumber));
+    if (isAutoClick) {
+      isIncrease
+        ? setCurrentNumber((currentNumber += +counterNumber))
+        : setCurrentNumber((currentNumber += -counterNumber));
+    }
   };
 
   return (
     <div>
       <p>Current : {currentNumber}</p>
       <p>
-        {textIncOrDec} : {counterNumber}
+        {textPages} : {counterNumber}
       </p>
       <input type="text" value={counterNumber} onChange={handleChange} />
-      <button onClick={changeСounter}>Execute {textIncOrDec}</button>
+      <button onClick={changeСounter}>Execute {textPages}</button>
+      <button onClick={() => setIsIncrease(!isIncrease)}>{textBtn}</button>
+      <input value={intervalTime} onChange={hendleIntervalTime} />
+      <button onClick={() => setIsAutoClick(!isAutoClick)}>Auto Click</button>
     </div>
   );
 }
